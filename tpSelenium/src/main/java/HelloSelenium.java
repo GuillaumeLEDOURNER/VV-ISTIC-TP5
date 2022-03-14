498e465905a118dfff510dc24c17c6e75b7bd475
Alexys Guerin <alexys.guerin@dolmen-tech.com>
2022-03-14 18:25:01 +0100

    find a random link and cheat on it

--- tpSelenium/src/main/java/HelloSelenium.java
+++ tpSelenium/src/main/java/HelloSelenium.java
@@ -1,21 +1,28 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import java.time.Duration;
+import java.util.List;
+import java.util.Random;
 
 public class HelloSelenium {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver","tpSelenium/chromedriverMAC");
+        System.setProperty("webdriver.chrome.driver","tpSelenium/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
-        driver.get("https://google.com");
+        driver.get("https://www.wikipedia.org/");
 
-        driver.getTitle(); // => "Google"
+        List<WebElement> allLinks = driver.findElements(By.xpath("//a[@href]"));
+        Random r = new Random();
+        System.out.println(allLinks.size());
+        int i = r.nextInt(allLinks.size());
+        driver.get(allLinks.get(i).getAttribute("href"));
+        //allLinks.forEach((v)-> System.out.println(v.getAttribute("href")));
+        /*driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
 
         WebElement searchBox = driver.findElement(By.name("q"));
         WebElement searchButton = driver.findElement(By.name("btnK"));
@@ -24,7 +31,7 @@ public class HelloSelenium {
         searchButton.click();
 
         searchBox = driver.findElement(By.name("q"));
-        searchBox.getAttribute("value"); // => "Selenium"
+        searchBox.getAttribute("value"); */// => "Selenium"
 
         driver.quit();
     }
