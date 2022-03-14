147ce2768476fcc1c914bfe7082a4117409444b0
Alexys Guerin <alexys.guerin@dolmen-tech.com>
2022-03-14 17:47:10 +0100

    add first selenium test

---
+++ tpSelenium/src/main/java/HelloSelenium.java
@@ -0,0 +1,31 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class HelloSelenium {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","tpSelenium/chromedriverMAC");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://google.com");
+
+        driver.getTitle(); // => "Google"
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
+
+        WebElement searchBox = driver.findElement(By.name("q"));
+        WebElement searchButton = driver.findElement(By.name("btnK"));
+
+        searchBox.sendKeys("Selenium");
+        searchButton.click();
+
+        searchBox = driver.findElement(By.name("q"));
+        searchBox.getAttribute("value"); // => "Selenium"
+
+        driver.quit();
+    }
+}
